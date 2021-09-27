ac17aa67c9aae938c767619b8a7c4a428213f54a
janayAl <jsallen298705@gmail.com>
2021-09-26 20:59:03 -0500

    updated intern and employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,34 +2,34 @@ const { TestWatcher } = require("jest");
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
-  // describe("Initialization", () => {
+  describe("Initialization", () => {
     it("should create a new Employee Class", () => {
       const obj = new Employee("Janay", 0, "nunnya@test.com");
-    
+
       expect(obj instanceof Employee).toEqual(true);
     });
 
     it("should return Employee as a role", () => {
       const obj = new Employee("Janay", 0, "nunnya@test.com");
-     
+
       expect(obj.getRole()).toEqual("Employee");
     })
 
-    it ("Should return Janay as a name.", () =>{
+    it("Should return Janay as a name.", () => {
       const obj = new Employee("Janay", 0, "nunnya@test.com");
 
       expect(obj.getName()).toEqual("Janay");
     })
 
-    it ("Should return 0 as Employee id.", () =>{
+    it("Should return 0 as Employee id.", () => {
       const obj = new Employee("Janay", 0, "nunnya@test.com");
-      expect (obj.getId()).toEqual(0);
+      expect(obj.getId()).toEqual(0);
     })
 
     it("Should return nunnya@test.com" as email.", () =>{
       const obj = new Employee("Janay", 0, "nunnya@test.com");
 
-      expect(obj.getEmail()).toEqual("nunnya@test.com");
+    expect(obj.getEmail()).toEqual("nunnya@test.com");
 
-    })
   })
+})
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,13 +1,15 @@
-const Employee = require("../lib/Employee");
+const { TestWatcher } = require("jest");
+const Intern = require("../lib/Intern");
+const Employee = require("../lib/Intern");
 
 describe("Intern", () => {
   describe("Initialization", () => {
     it("should create a new Intern Class", () => {
-      const obj = new Employee("Janay", 0, "nunnya@test.com");
+      const obj = new Intern("Janay", 0, "nunnya@test.com");
 
-      expect(obj instanceof Employee).toEqual(true);
+      expect(obj instanceof Intern).toEqual(true);
     });
 
     it("should return Intern as a role", () => {
-      const obj = new Intern ("Janay", 0, "nunnya@test.com")
-});
+      const obj = new Intern("Janay", 0, "nunnya@test.com")
+    });
