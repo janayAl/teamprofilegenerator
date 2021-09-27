99787f7a270b9bbcd83781cd9700cbac8666c615
janayAl <jsallen298705@gmail.com>
2021-09-26 21:02:21 -0500

    replaced teh worrd emmployee to engineer in the engineer test file

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,14 +1,14 @@
 const Employee = require("../lib/Employee");
 const { TestWatcher } = require("jest");
 
-describe("Employee", () => {
+describe("Engineer", () => {
   describe("Initialization", () => {
-    it("should create a new Employee Class", () => {
-      const obj = new Employee("Janay", 0, "nunnya@test.com");
+    it("should create a new Engineer Class", () => {
+      const obj = new Engineer("Janay", 0, "nunnya@test.com");
 
-      expect(obj instanceof Employee).toEqual(true);
+      expect(obj instanceof Engineer).toEqual(true);
     });
 
-    it("should return Employee as a role", () => {
-      const obj = new Employee("Janay", 0, "nunnya@test.com")
+    it("should return Engineer as a role", () => {
+      const obj = new Engineer("Janay", 0, "nunnya@test.com")
     });
